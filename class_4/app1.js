9fe6732d391f72bac1fedb04126c837929e12f71
ridohyun <ri.dohyun@gmail.com>
2021-01-07 20:51:44 +0900

    Upload files

---
+++ class_4/app1.js
@@ -0,0 +1,11 @@
+console.log("1+2");
+console.log("정글아카데미");
+console.log(10+10+'정글아카데미'+10);
+
+function textFn(){
+    let arr = [1,"가","10",true];
+    arr.push(20);
+    arr.push("나");
+    console.log(arr);
+}
+textFn();
---
+++ class_4/app2.js
@@ -0,0 +1,3 @@
+let log = true;
+if(log) console.log(log);
+else console.log(false);
